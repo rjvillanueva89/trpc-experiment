b53bce578c25379dde020fe60a9a11241862926c
RJ Villanueva <rj.villanueva89@gmail.com>
2023-08-25 09:25:24 +0800

    added query with input sample

--- src/app/_components/todo-list.tsx
+++ src/app/_components/todo-list.tsx
@@ -4,7 +4,9 @@ import { trpc } from "../_trpc/client"
 
 export const TodoList = () => {
   const getTodos = trpc.todos.getAll.useQuery()
+  const todo = trpc.todos.getById.useQuery({ id: "someId" })
 
   console.log(getTodos.data)
+  console.log(todo.data)
   return <></>
 }
--- src/server/routers/todos.ts
+++ src/server/routers/todos.ts
@@ -1,7 +1,13 @@
+import { z } from "zod"
 import { publicProcedure, router } from "../trpc"
 
 export const todosRouter = router({
   getAll: publicProcedure.query(async () => {
     return [10, 20, 30]
   }),
+  getById: publicProcedure
+    .input(z.object({ id: z.string().optional() }))
+    .query(async ({ input }) => {
+      return input.id
+    }),
 })
